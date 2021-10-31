9f6e6a53310c2f61e1fd18bb65df382af644cb44
Mateusz <mateuszzygadlo00@gmail.com>
2021-10-31 17:02:31 +0100

    Displaying library data

--- controllers/authorController.js
+++ controllers/authorController.js
@@ -1,4 +1,6 @@
 const Author = require('../models/author');
+const Book = require('../models/book');
+const async = require('async');
 
 exports.author_list = (req, res, next) => {
     Author.find()
@@ -10,10 +12,27 @@ exports.author_list = (req, res, next) => {
     });
 };
 
-exports.author_detail = function(req, res) {
-    res.send('NOT IMPLEMENTED: Author detail: ' + req.params.id);
-};
+exports.author_detail = function(req, res, next) {
+    async.parallel({
+        author: function(callback) {
+            Author.findById(req.params.id)
+              .exec(callback)
+        },
+        authors_books: function(callback) {
+          Book.find({ 'author': req.params.id },'title summary')
+          .exec(callback)
+        },
+    }, function(err, results) {
+        if (err) { return next(err); }
+        if (results.author==null) { 
+            var err = new Error('Author not found');
+            err.status = 404;
+            return next(err);
+        }
 
+        res.render('author_detail', { title: 'Author Detail', author: results.author, author_books: results.authors_books } );
+    });
+};
 exports.author_create_get = function(req, res) {
     res.send('NOT IMPLEMENTED: Author create GET');
 };
--- models/bookinstance.js
+++ models/bookinstance.js
@@ -36,5 +36,4 @@ BookInstanceSchema
       return DateTime.fromJSDate(this.due_back).toLocaleString(DateTime.DATE_MED);
     });
 
-//Export model
 module.exports = mongoose.model('BookInstance', BookInstanceSchema);
