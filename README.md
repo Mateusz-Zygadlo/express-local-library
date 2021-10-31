bddb1cfee8aa45b52724f68dcb579f664066d2a5
Mateusz <mateuszzygadlo00@gmail.com>
2021-10-31 20:54:43 +0100

    Adding README.md

---
+++ README.md
@@ -0,0 +1,5 @@
+Node Express
+
+I created project with the mdn tutorial The Local Library website
+I was unable to do the delete pages and update pages tasks
+I also didn't upload the application to the server
--- app.js
+++ app.js
@@ -8,8 +8,11 @@ const mongodbUrl = require('./databaseUrl');
 const indexRouter = require('./routes/index');
 const usersRouter = require('./routes/users');
 const catalogRouter = require('./routes/catalog');
+const compression = require('compression');
+const helmet = require('helmet');
 
 const app = express();
+
 
 //Set up mongoose connection
 const mongoose = require('mongoose');
@@ -29,12 +32,14 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+app.use(compression());
+app.use(helmet());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
-
 app.use('/users', usersRouter);
 app.use('/catalog', catalogRouter); 
+
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
